390a8bef8b1f832c28de831e1a94b766c616eee5
Krishnakant Waghmode <ashutoshwaghmode@gmail.com>
2022-11-28 21:00:03 +0530

    My First Commit

---
+++ Velocity_Mini_Project/src/com/velocity/miniproject/Main.java
@@ -0,0 +1,10 @@
+package com.velocity.miniproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Velocity_Mini_Project/src/com/velocity/miniproject/StudentEnrollment.java
@@ -0,0 +1,5 @@
+package com.velocity.miniproject;
+
+public class StudentEnrollment {
+
+}
